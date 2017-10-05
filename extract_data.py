b5798a0a3a45ddc1430db8a6cde85a37aa8eb60a
José Gómez-Dans <jgomezdans@gmail.com>
2017-10-05 17:24:38 +0100

    More bells and whistles
    Now deals with MCD43, PSF integration and subsetting.

--- extract_data.py
+++ extract_data.py
@@ -21,6 +21,8 @@ import pylab as plt
 
 from helper_functions import reproject_image_to_master, hplot
 from helper_functions import parse_xml, load_emulator_training_set
+import kernels
+from spatial_mapping import *
 
 parent_folder = "/data/selene/ucfajlg/S2_AC/TeleSpazio/" + \
                 "ftp.telespazio.fr/outgoing/L2A_Products/"
@@ -65,6 +67,7 @@ class TeleSpazioComparison(object):
         self.tile = tile
         self.l1c_files = self._get_safe_files("L1C")
         self.__find_l2a_data()
+        self.modis_files(tile, site)
         
     def __find_l2a_data(self):
         self.l2a_files = self._get_safe_files("L2A")
@@ -281,7 +284,7 @@ class TeleSpazioComparison(object):
         # ensure odir exists
         if not os.path.exists(odir): os.makedirs(odir)
 
-        fname = odir+'/'+'SEN2COR_%s_%s_%s_%s'%(self.site, self.tile, the_date.strftime("%Y-%m-%d %H:%M:%S"), band)
+        fname = odir+'/'+'SEN2COR_%s_%s_%s_%s'%(self.site, self.tile, the_date.strftime("%Y-%m-%d_%H:%M"), band)
 
         toa_set = self.get_l1c_data(the_date)
         boa_set = self.get_l2_data(the_date)
@@ -363,209 +366,108 @@ class TeleSpazioComparison(object):
         plt.close() 
         return model_ransac, retval
         
+    def compare_boa_refl_MCD43(self, the_date, the_band):
+        xstd = 30.
+        ystd = 30.
+        angle = 0.
+        print "NOTE!! Gaussian not optimised for x- and y-shift!"
+        psf = gaussian(xstd, ystd, angle, norm = True)
+        mapping_arrays = Find_corresponding_pixels(ts.l2a_datasets[the_date][the_band+1])
+        tile = mapping_arrays.keys()[0] # Only 1 tile...
+        self.modis_files (tile, self.site)
+        rho_boa_hires = gdal.Open(
+            self.l2a_datasets[the_date][the_band]).ReadAsArray()/10000. # 
+        rho_boa_s2 = PSF_convolve(rho_boa_hires, psf, mapping_arrays[tile][0], 
+                                    mapping_arrays[tile][1])
+        s2_mask = np.isfinite(rho_boa_s2)
+        min_y, max_y = np.nonzero(s2_mask)[0].min(), \
+            np.nonzero(s2_mask)[0].max()
+        min_x, max_x = np.nonzero(s2_mask)[1].min(), \
+            np.nonzero(s2_mask)[1].max()
 
-    def get_modis_files(self, site):
-        """Gets the MODIS files. You get in return a dictionary
-        with the same keys as the L1 andHCD43A2 products.
-        """
-        layer_selector = {
-            'MCD43A1':['HDF4_EOS:EOS_GRID:"%s":MOD_Grid_BRDF:BRDF_Albedo_Parameters_Band'+"%d"%(b+1) for b in xrange(7)],
-            'MCD43A2':['HDF4_EOS:EOS_GRID:"%s":MOD_Grid_BRDF:BRDF_Albedo_Uncertainty','HDF4_EOS:EOS_GRID:"%s":MOD_Grid_BRDF:Snow_BRDF_Albedo']}
-        modis_mapper = {}
-        for product in [ "MCD43A1", "MCD43A2"]:
-            modis_mapper[product] = {}
-            files = glob.glob(
-                os.path.join(MODIS_DIR, site) + 
-                "/%s*.hdf" % product)
-            files.sort()
-            modis_dates_t = [f.split("/")[-1].split(".")[1][1:] 
-                                for f in files]
-            modis_dates = [datetime.datetime.strptime(d, 
-                        "%Y%j")
-                        for d in modis_dates_t]
+        modis_prediction = self.predict_boa_refl_MCD43(the_date, the_band)
+        return rho_boa_s2[min_y:max_y, min_x:max_x], \
+            modis_prediction[min_y:max_y, min_x:max_x]
+        
+
+
+    def predict_boa_refl_MCD43(self, the_date, the_band):
+        # find modis matching dataset
+        print "FIX band_equivalences!!!!!!!!!!"
+        band_equivalences = [ [[1, 1.]], [[2, 1.]], [[3, 1.]], 
+                                [[4, 1.]], [[5, 1.]], [[6, 1.]], [[7, 1.]] ]
+        sza, saa, vza, vaa = self.get_l1c_angles(the_date)
+        raa = vaa - saa
+        K = kernels.Kernels( vza, sza, raa,
+            LiType='Sparse', doIntegrals=False, 
+            normalise=1, RecipFlag=True, RossHS=False, MODISSPARSE=True,
+            RossType='Thick' )
+        kk = np.array([1., K.Ross[0], K.Li[0]])
+        
+        k = min(self.modis_filelist.keys(), 
+                key=lambda x: abs(x - the_date))
+        pred_boa_refl = np.zeros((2400, 2400))
+        for band, w in band_equivalences[the_band]:
             
-            for s2_date in self.l2a_datasets.iterkeys():
-                for i, modis_date in enumerate(modis_dates):
-                    if modis_date.date() == s2_date.date():
-                        master=self.get_l2_data(s2_date).scl_20
-                        # Now, reproject/crop all layers for
-                        # this product...
-                        fnames = []
-                        for layer in layer_selector[product]:
-                            fnames.append(
-                                reproject_image_to_master (
-                                master, files[i], layer))
+            g = gdal.Open('HDF4_EOS:EOS_GRID:"%s":MOD_Grid_BRDF:BRDF_Albedo_Parameters_Band%d' % 
+                        (self.modis_filelist["MCD43A1"][k], band))
+            kernel_weights = g.ReadAsArray()/10000.
+            g = gdal.Open('HDF4_EOS:EOS_GRID:"%s":MOD_Grid_BRDF:BRDF_Albedo_Band_Quality_Band%d' % 
+                        (self.modis_filelist["MCD43A2"][k], band))
+            qa = g.ReadAsArray()
+            print "Only best quality MCD43 samples selected"
+            kernel_weights = np.where(qa == 0, kernel_weights, np.nan)
 
-                        modis_mapper[product][s2_date] = fnames
-        return modis_mapper
+            pred_boa_refl += w*np.sum(kernel_weights*kk[:, None, None], axis=0)
+        return pred_boa_refl
+
+    def modis_files (self, tile, site, folder="/storage/ucfajlg/S2_AC/MCD43/"):
+        self.modis_filelist = {}
+        for product in ["MCD43A1", "MCD43A2"]:
+            self.modis_filelist[product] = {}
+            files = glob.glob ( os.path.join( folder, site, 
+                                             "%s*%s*hdf"%(product, tile)))
+            
+            for fich in files:
+                date_str = os.path.basename(fich).split(".")[1]
+                the_date = datetime.datetime.strptime(date_str, "A%Y%j")
+                self.modis_filelist[product][the_date] = fich
 
 
-class CNESComparison(TeleSpazioComparison):
-    """A Class to do comparisons of the CNES L2A product."""
-    def __init__ (self, site, tile):
-        # The parent class creator uses the TeleSpazio data to fetch the L1C
-        # products
 
-        TeleSpazioComparison.__init__(self, site, tile)
-        self.__find_l2a_data()
-        # self.l1c_files is now defined, and starts to look for L2A products
-        
-    ###def __find_l2a_data(self):
-        ###self.l2a_files = self._get_safe_files("L2A")
-        ###self.l1c_datasets = {}
-        ###self.l2a_datasets = {}
-        ###for the_date in self.l1c_files.iterkeys():
-            ###retval = self.get_l1c_data(the_date)
-            ###if retval is None:
-                ###continue
-                #### No tile found
-            ###self.l1c_datasets[the_date]=retval
-            ###self.l2a_datasets[the_date] = self.get_l2_data(
-                ###the_date)
-    def __find_l2a_data(self):         
-        granules = glob.glob( os.path.join(CNES_DIR, "SENTINEL2A*"))
-        granules.sort()
-        self.l2a_files = {}
-        for granule in granules:
-            if granule.find(self.tile) >= 0: # We have the tile!
-                
-                tstring = granule.split("/")[-1].split("_")[1]
-                key = datetime.datetime.strptime( tstring, 
-                                "%Y%m%d-%H%M%S-%f").replace(microsecond=0)
-                print "Dealing with iamge acquired on %s" % key
-                
-                x = self._unpack_data(granule)
-                self.l2a_files[key] = x
-       
-    def _unpack_data(self, granule, product=None):
-        # CNES data are zipped up. This unzips the files up and returns a list 
-        # of files back
-        
-        zipname = os.path.join(granule, granule.split("/")[-1]+".zip")
-        zipper = zipfile.ZipFile(zipname)
-        
-        if not os.path.exists(os.path.join(granule, "MASKS")):
-            # Uncompress data
-            print "Uncompressing zipfile"
-            zipper.extractall(CNES_DIR)
-        files = []
-        tags = []
-        for product in ["SRE", "ATB", "SAT", "CLM", "MG2"]:
-            for fich in zipper.namelist():
-                if fich.find(product) >= 0:
-                    fname = fich.split("/")[-1]
-                    tag = "_".join(fname.replace(".tif","").split("_")[-2:]).lower()
-                    if product == "SRE":
-                        tag = tag.replace("sre_", "")
-                    tags.append(tag)
-                    files.append ( os.path.join(CNES_DIR, fich))
-        zipper.close()
-        
-        files2 = []
-        tago = ('b2 b3 b4 b5 b6 b7 b8 b8a b11 b12 atb_r1 ' + 
-            'atb_r2 sat_r1 sat_r2 clm_r1 clm_r2 mg2_r1 mg2_r2').split()
-        for t in tago:
-                files2.append(files[tags.index(t)])
-        files = CNES_BOA_set(*files2)
-        return files
-        
-    def get_transform(self, the_date, band, mask="L2",
-                      sub=10, nv=200, lw=2, odir='figures',
-                      apply_model=False, plausible=True):
-
-        # ensure odir exists
-        if not os.path.exists(odir): os.makedirs(odir)
-
-        fname = odir+'/'+'MAJA_%s_%s'%(the_date.strftime("%Y-%m-%d %H:%M:%S"), band)
-
-        toa_set = self.get_l1c_data(the_date)
-        boa_set = self.get_l2_data(the_date)
-        if toa_set is None or boa_set is None:
-            print "No TILEs found for %s" % the_date
-            return None
-        g = gdal.Open(toa_set[TOA_list.index(band)])
-        toa_rho = g.ReadAsArray()
-        g = gdal.Open(boa_set[CNES_BOA_list.index(band)])
-        boa_rho = g.ReadAsArray()
-        if mask == "L2":
-            # TODO NEEDS WORK -> use clm_r1/r2 filter veg?
-            # if mg2, times by 01000000 if 0 no cloud 
-            print "Using L2A product mask"
-            if band in ["B02", "B03", "B04", "B08"]:
-                g = gdal.Open(boa_set.mg2_r1)
-                c = g.ReadAsArray()
-                mask = np.bitwise_and(2, c) == 2
-                
-            elif band in ["B05", "B06", "B07", "B11", "B12", "B8A"]:
-                g = gdal.Open(boa_set.mg2_r2)
-                c = g.ReadAsArray()
-                mask = np.bitwise_and(2, c) == 2
-                
-
-        else:
-            mask_toa = np.logical_or(toa_rho == 0,
-                                    toa_rho > 20000)
-            mask_boa = np.logical_or(boa_rho == 0,
-                                    boa_rho > 20000)
-            mask = mask_boa*mask_toa
-        toa_rho = toa_rho/10000.
-        boa_rho = boa_rho/10000.
-        
-        x = boa_rho[~mask][::sub]
-        y = toa_rho[~mask][::sub]
-        
-        vmin = np.min([0.0,np.min(x),np.min(y)])
-        vmax = np.max([1.0,np.max(x),np.max(y)])
-
-        ns = x.size
-        # robust linear model fit
-        model = linear_model.LinearRegression(n_jobs=-1)
-        model_ransac = linear_model.RANSACRegressor(model)
-        model_ransac.fit(y.reshape(ns,1), x) 
-        inlier_mask = model_ransac.inlier_mask_
-        outlier_mask = np.logical_not(inlier_mask)
-        line_X = np.arange(vmin,vmax,(vmax-vmin)/nv)       
-        line_y_ransac = model_ransac.predict(line_X[:, np.newaxis])
-        xlim = ylim = [vmin,vmax]
-
-        hplot(x, y, new=True,xlim=xlim,ylim=ylim)
-        xyrange = xlim,ylim
-        plt.xlim(xlim)
-        plt.plot(xyrange[0],xyrange[1],'g--', label='1:1 line')
-        plt.plot(line_y_ransac, line_X, color='red', linestyle='-',linewidth=lw, label='RANSAC regressor') 
-        plt.xlabel('BOA reflectance Band %s'%band)
-        plt.ylabel('TOA reflectance Band %s'%band)
-
-        a,b = model_ransac.predict(np.array([0.,1.])[:, np.newaxis])
-        plt.title(the_date.strftime("%Y-%m-%d %H:%M:%S") + \
-		'\nBOA(%s) = %.3f + %.3f TOA(%s)'%(band,a,b-a,band) + \
-		'\nTOA(%s) = %.3f + %.3f BOA(%s)'%(band,a/(a-b),1./(b-a),band))
-
-        if vmax > 1:
-            plt.plot(xlim,[1.0,1.0],'k--',label='TOA reflectance == 1')
-        if plausible:
-            boa_emu, toa_emu = load_emulator_training_set()
-            plt.plot(boa_emu, toa_emu[band], '+', markersize=3, c="cyan", label="Plausible")
-        plt.legend(loc='best')
-        plt.savefig(fname+'.scatter.pdf')
-        plt.close() 
-        if apply_model:
-            approx_boa_rho = model_ransac.predict(toa_rho[~mask].flatten()[:, 
-                                                                 np.newaxis])
-            retval = np.zeros_like (toa_rho)
-            retval[~mask] = approx_boa_rho
-        return model_ransac, retval
                 
         
-
+                
+            
+            
 if __name__ == "__main__":
+    from spatial_mapping import *
 
-    for (site,tile) in [ ["Ispra", "T32TMR"], 
-                        ["Pretoria", "35JPM"], ["Pretoria", "35JQM"]]:
+    for (site,tile) in [ ["Ispra", "T32TMR"]]:
+                        #["Pretoria_CSIR-DPSS", "35JPM"], 
+                        #["Pretoria_CSIR-DPSS", "35JQM"]]:
+    #for (site,tile) in [ 
+                        #["Pretoria_CSIR-DPSS", "35JPM"], 
+                        #["Pretoria_CSIR-DPSS", "35JQM"]]:
+
         ts = TeleSpazioComparison(site, tile)
-        for ii, the_date in enumerate( ts.l1c_datasets.iterkeys()):        
-            for band in TOA_list[1:9]:
-                retval = ts.get_transform(the_date, band, apply_model=True)
+        
+        for ii, the_date in enumerate( ts.l2a_datasets.iterkeys()):        
+            print the_date
+            
+            
+
+            
+            break
+            
+            
+            
+
+
+
+            #for band in TOA_list[1:9]:
+            #    retval = ts.get_transform(the_date, band, apply_model=True)
+            
         
     ######ts = TeleSpazioComparison("Pretoria", "35JPM")
     ######for ii, the_date in enumerate( ts.l1c_files.iterkeys()):
