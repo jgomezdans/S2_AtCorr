3eb8b6709d9baa98ede2b9da2ebca8045afd2139
José Gómez-Dans <jgomezdans@gmail.com>
2017-10-05 17:23:27 +0100

    Added a couple of extra funcs

--- helper_functions.py
+++ helper_functions.py
@@ -150,3 +150,85 @@ def parse_xml(filename):
                             vaa.append(float(r.text))
                             
     return sza, saa, np.mean(vza), np.mean(vaa)
+
+
+
+def Find_corresponding_pixels(H_res_fname, destination_res=500):
+    
+    '''
+    A function for the finding of corresponding pixels indexes 
+    in H (sentinel 2) and L (MODIS) resolution image.
+    
+    args:
+        H_res_fname -- the high resolution image filename, need to have geoinformation enbeded in the file
+        destination_res -- for the calculation of pixel number in one MODIS tile
+    return:
+        index: a dictionary contain both the MODIS tile name and pixels indexes
+    
+    '''
+    
+    if destination_res % 250 == 0:
+        pass
+    else:
+        print 'destination resolution can only be 250, 500 and 1000 !!!'
+        raise IOError
+
+    g = gdal.Open(H_res_fname)
+    geo_t = g.GetGeoTransform()
+    x_size, y_size = g.RasterXSize, g.RasterYSize
+
+    wgs84 = osr.SpatialReference( ) # Define a SpatialReference object
+    wgs84.ImportFromEPSG( 4326 ) # And set it to WGS84 using the EPSG code
+    H_res_geo = osr.SpatialReference( )
+    raster_wkt = g.GetProjection()
+    H_res_geo.ImportFromWkt(raster_wkt)
+    tx = osr.CoordinateTransformation(H_res_geo, wgs84)
+    # so we need the four corners coordiates to check whether they are within the same modis tile
+    (ul_lon, ul_lat, ulz ) = tx.TransformPoint( geo_t[0], geo_t[3])
+
+    (lr_lon, lr_lat, lrz ) = tx.TransformPoint( geo_t[0] + geo_t[1]*x_size, \
+                                          geo_t[3] + geo_t[5]*y_size )
+
+    (ll_lon, ll_lat, llz ) = tx.TransformPoint( geo_t[0] , \
+                                          geo_t[3] + geo_t[5]*y_size )
+
+    (ur_lon, ur_lat, urz ) = tx.TransformPoint( geo_t[0] + geo_t[1]*x_size, \
+                                          geo_t[3]  )
+
+    #print (ul_lon, ul_lat), (lr_lon, lr_lat), (ll_lon, ll_lat), (ur_lon, ur_lat)
+    # now its the s2 corners latitudes and longtitudes
+    s_dic ={'UR_LAT': ur_lat,
+           'UR_LON': ur_lon,
+           'LR_LAT': lr_lat,
+           'LR_LON': lr_lon,
+           'UL_LAT': ul_lat,
+           'UL_LON': ul_lon,
+           'LL_LAT': ll_lat,
+           'LL_LON': ll_lon}
+    s_cors = ulx, uly, lrx, lry, llx, lly, urx, ury = 0,0,x_size, y_size, x_size,0, 0, y_size 
+    s_corners = dict(zip(['ulx', 'uly', 'lrx', 'lry', 'llx', 'lly', 'urx', 'ury'], s_cors))
+    
+    a0, b0 = None, None
+    corners = [(ul_lon, ul_lat), (lr_lon, lr_lat), (ll_lon, ll_lat), (ur_lon, ur_lat)]
+    tiles = []
+    for i,j  in enumerate(corners):
+        h,v = mtile_cal(j[1], j[0])
+        if (h==a0) &(v==b0):
+            pass
+        else:
+            tiles.append([i,h,v]) # 0--ul;1--lr;2--ll;3--ur
+            a0, b0 = h,v
+    
+    # The modis defults
+    pix_num = 4800/(destination_res/250)
+    
+    inds = {}
+    for i in tiles:
+        latitudes, longtitudes = get_Mpix_wgs(h,v, pix_num).T[[1,0],]
+        cors = bilineanr([latitudes, longtitudes], s_dic, s_corners)
+        hinds =np.array([cors[0][(cors[0]>=0)&(cors[0]<x_size)&(cors[1]>=0)&(cors[1]<y_size)],
+                     cors[1][(cors[0]>=0)&(cors[0]<x_size)&(cors[1]>=0)&(cors[1]<y_size)]]).astype(int)
+        minds = np.where(((cors[0]>=0)&(cors[0]<x_size)&(cors[1]>=0)&(cors[1]<y_size)).reshape((pix_num,pix_num)))
+        inds['h%02dv%02d'%(i[1],i[2])] = [hinds, np.array(minds)]
+
+    return inds
